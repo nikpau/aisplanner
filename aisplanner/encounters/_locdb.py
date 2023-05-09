e656d7fe9c30c0cb2ab9d79ec76edd6322f931c4
nikpau <n.paulig@web.de>
2023-05-09 13:05:38 +0200

    update locdb

--- aisplanner/encounters/_locdb.py
+++ aisplanner/encounters/_locdb.py
@@ -1,33 +1,33 @@
 """
 Database of suitable locations for for COLREGS-relevant encounters.
 """
-from pytsa.targetship import BoundingBox
+from pytsa.structs import LatLonBoundingBox
 from dataclasses import dataclass, fields
 
 @dataclass
 class LocationDatabase:
     # Frederikshavn to Gothenburg
-    fre_got: BoundingBox = BoundingBox(
+    fre_got: LatLonBoundingBox = LatLonBoundingBox(
         LATMIN=57.378,
         LATMAX=57.778,
         LONMIN=10.446,
         LONMAX=11.872
     )
-    elbe_approach: BoundingBox = BoundingBox(
+    elbe_approach: LatLonBoundingBox = LatLonBoundingBox(
         LATMIN=53.9807,
         LATMAX = 54.049,
         LONMIN=7.487,
         LONMAX=7.7734
     )
     # Helsingor to Helsingborg
-    hel_hel: BoundingBox = BoundingBox(
+    hel_hel: LatLonBoundingBox = LatLonBoundingBox(
         LATMIN=55.998,
         LATMAX=56.064,
         LONMIN=12.560,
         LONMAX=12.745
     )
     # Hirtsals to Kristiansand
-    hir_krs: BoundingBox = BoundingBox(
+    hir_krs: LatLonBoundingBox = LatLonBoundingBox(
         LATMIN=57.393,
         LATMAX=58.240,
         LONMIN=7.280,
