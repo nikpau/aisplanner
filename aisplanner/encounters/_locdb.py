c98646de493df46fcc1d5fb8e6d7595ed6a3ca13
nikpau <n.paulig@web.de>
2023-06-12 13:36:21 +0200

    replace name

--- aisplanner/encounters/_locdb.py
+++ aisplanner/encounters/_locdb.py
@@ -12,14 +12,14 @@ class LocationDatabase:
         LATMAX=57.778,
         LONMIN=10.446,
         LONMAX=11.872,
-        __name__="fre_got"
+        name="fre_got"
     )
     elbe_approach: LatLonBoundingBox = LatLonBoundingBox(
         LATMIN=53.9807,
         LATMAX = 54.049,
         LONMIN=7.487,
         LONMAX=7.7734,
-        __name__="elbe_approach"
+        name="elbe_approach"
     )
     # Helsingor to Helsingborg
     hel_hel: LatLonBoundingBox = LatLonBoundingBox(
@@ -27,7 +27,7 @@ class LocationDatabase:
         LATMAX=56.064,
         LONMIN=12.560,
         LONMAX=12.745,
-        __name__="hel_hel"
+        name="hel_hel"
     )
     # Hirtsals to Kristiansand
     hir_krs: LatLonBoundingBox = LatLonBoundingBox(
@@ -35,7 +35,7 @@ class LocationDatabase:
         LATMAX=58.240,
         LONMIN=7.280,
         LONMAX=9.995,
-        __name__="hir_krs"
+        name="hir_krs"
     )
     # Return all locations as a list
     @classmethod
