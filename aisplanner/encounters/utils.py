2419f28cb9a8765526c9a062bed8381ced9166b8
nikpau <n.paulig@web.de>
2023-06-15 08:17:46 +0200

    split up plotting funcs

--- aisplanner/encounters/utils.py
+++ aisplanner/encounters/utils.py
@@ -3,6 +3,19 @@ from pytsa.targetship import TargetVessel
 from itertools import permutations
 import pickle
 from ..misc import logger
+from pathlib import Path
+from typing import Union
+
+RESDIR = Path("results")
+REMOTEHOST = "taurus.hrsk.tu-dresden.de"
+REMOTEDIR = Path("/warm_archive/ws/s2075466-ais/decoded/jan2020_to_jun2022")
+
+def load_results(filename: Union[str, Path]):
+    if not isinstance(filename, Path):
+        filename = Path(filename)
+    with open(RESDIR / filename, "rb") as f:
+        results = pickle.load(f)
+    return results
 
 def save_overlapping_trajectories(tgts: list[TargetVessel], filename: str):
     out = set()
@@ -20,4 +33,13 @@ def save_overlapping_trajectories(tgts: list[TargetVessel], filename: str):
     with open(filename, "wb") as f:
         pickle.dump(out, f)
     return
+
+def plot_encounter(v1: TargetVessel,v2: TargetVessel):
+        tm = TrajectoryMatcher(v1,v2)
+        if not tm.disjoint_trajectories and tm.overlapping_trajectories:
+            print(f"Plotting encounter for {v1.mmsi} and {v2.mmsi}")
+            tm.observe_interval(10).plot(every=6, path="out/plots/")
+            return
+        print(f"Disjoint trajectories for {v1.mmsi} and {v2.mmsi}")
+        return
     
