7170a2d222820e3c238bb698d2872eea92830eb6
nikpau <n.paulig@web.de>
2023-06-15 20:18:31 +0200

    fix visual debugger

--- aisplanner/encounters/tplot.py
+++ aisplanner/encounters/tplot.py
@@ -4,16 +4,17 @@ Module for plotting colregs encounters found via filter.py
 """
 from itertools import permutations
 
-from pytsa.targetship import TargetVessel
-from .utils import load_results, plot_encounter
+from aisplanner.encounters.utils import load_results, plot_encounter, OverlappingPair
 
 if __name__ == "__main__":
     # Load results
-    res: list[TargetVessel] = load_results("2021_hel_hel.tr")
+    res: list[OverlappingPair] = load_results("results/overlapping/2021_fre_got_overlapping.tr")
     print(len(res))
-    for v1,v2 in permutations(res,2):
+    for vobj in res:
+        if vobj.v1.mmsi == vobj.v2.mmsi:
+            continue
         try:
-            plot_encounter(v1,v2)
+            plot_encounter(*vobj())
         except Exception as e:
             print(e)
             continue
