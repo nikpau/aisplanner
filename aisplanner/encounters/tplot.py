1e90684c5578e26ee91cfe63f3ae1573c048c0a3
nikpau <n.paulig@web.de>
2023-06-16 13:46:30 +0200

    refactor for strcutre

--- aisplanner/encounters/tplot.py
+++ aisplanner/encounters/tplot.py
@@ -2,16 +2,16 @@
 Trajectory plotting functions
 Module for plotting colregs encounters found via filter.py
 """
-from itertools import permutations
-
-from aisplanner.encounters.utils import load_results, plot_encounter, OverlappingPair
+from aisplanner.encounters.utils import (
+    load_results, plot_encounter, OverlappingPair
+)
 
 if __name__ == "__main__":
     # Load results
     res: list[OverlappingPair] = load_results("results/overlapping/2021_fre_got_overlapping.tr")
     print(len(res))
     for vobj in res:
-        if vobj.v1.mmsi == vobj.v2.mmsi:
+        if vobj.same_mmsi():
             continue
         try:
             plot_encounter(*vobj())
