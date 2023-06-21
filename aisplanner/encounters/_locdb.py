3bfc5e5bf7af978ffa3e1d7ef8289cf92268625d
nikpau <n.paulig@web.de>
2023-06-21 16:11:02 +0200

    names in post init

--- aisplanner/encounters/_locdb.py
+++ aisplanner/encounters/_locdb.py
@@ -11,32 +11,36 @@ class LocationDatabase:
         LATMIN=57.378,
         LATMAX=57.778,
         LONMIN=10.446,
-        LONMAX=11.872,
-        name="Frederikshavn_to_Gothenburg",
+        LONMAX=11.872
     )
     elbe_approach: LatLonBoundingBox = LatLonBoundingBox(
         LATMIN=53.9807,
         LATMAX = 54.049,
         LONMIN=7.487,
-        LONMAX=7.7734,
-        name="Elbe_approach",
+        LONMAX=7.7734
     )
     # Helsingor to Helsingborg
     hel_hel: LatLonBoundingBox = LatLonBoundingBox(
         LATMIN=55.998,
         LATMAX=56.064,
         LONMIN=12.560,
-        LONMAX=12.745,
-        name="Helsingor_to_Helsingborg",
+        LONMAX=12.745
     )
     # Hirtsals to Kristiansand
     hir_krs: LatLonBoundingBox = LatLonBoundingBox(
         LATMIN=57.393,
         LATMAX=58.240,
         LONMIN=7.280,
-        LONMAX=9.995,
-        name="Hirtsals_to_Kristiansand",
+        LONMAX=9.995
     )
+
+    def __post_init__(self):
+        self.fre_got.name = "Frederikshavn_to_Gothenburg"
+        self.elbe_approach.name = "Elbe_approach"
+        self.hel_hel.name = "Helsingor_to_Helsingborg"
+        self.hir_krs.name = "Hirtsals_to_Kristiansand"
+        
+
     # Return all locations as a list
     @classmethod
     def all(cls,utm=False):
