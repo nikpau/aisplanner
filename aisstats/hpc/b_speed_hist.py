7f0818c2693e1e0c02b06fd351cf8b4044571dc3
nikpau <n.paulig@web.de>
2023-12-13 10:58:08 +0100

    dt

--- aisstats/hpc/b_speed_hist.py
+++ aisstats/hpc/b_speed_hist.py
@@ -3,18 +3,41 @@ HPC script for calculating descriptive data
 for the AIS data set
 """
 from pathlib import Path
-from pytsa.decode import filedescriptor as fd
-import numpy as np
-import pandas as pd
-import multiprocessing as mp
 from aisplanner.encounters.main import NorthSea
 from aisstats.errchecker import plot_speed_histogram
 from pathlib import Path
-from pytsa import SearchAgent, TimePosition, ShipType
+from pytsa import SearchAgent, TimePosition
+import ciso8601
+
+
+def _date_transformer(datefile: Path) -> float:
+    return ciso8601.parse_datetime(datefile.stem.replace("_", "-"))
 
 DYNAMIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022').glob("2021*.csv"))
 STATIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022/msgtype5').glob("2021*.csv"))
 
+if len(DYNAMIC_MESSAGES) != len(STATIC_MESSAGES):
+
+    print(
+        "Number of dynamic and static messages do not match."
+        f"Dynamic: {len(DYNAMIC_MESSAGES)}, static: {len(STATIC_MESSAGES)}\n"
+        "Processing only common files."
+    )
+    # Find the difference
+    d = set([f.stem for f in DYNAMIC_MESSAGES])
+    s = set([f.stem for f in STATIC_MESSAGES])
+    
+    # Find all files that are in d and s
+    common = d.intersection(s)
+    common = list(common)
+    
+    # Remove all files that are not in common
+    DYNAMIC_MESSAGES = [f for f in DYNAMIC_MESSAGES if f.stem in common]
+    STATIC_MESSAGES = [f for f in STATIC_MESSAGES if f.stem in common]
+    
+# Sort the files by date
+DYNAMIC_MESSAGES = sorted(DYNAMIC_MESSAGES, key=_date_transformer)
+STATIC_MESSAGES = sorted(STATIC_MESSAGES, key=_date_transformer)
 
 SEARCHAREA = NorthSea
 SA = SearchAgent(
