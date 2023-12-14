0dab2bfb06b218bfa3188ae230b9143af2a72e96
nikpau <n.paulig@web.de>
2023-12-14 11:59:57 +0100

    coast

--- aisstats/hpc/b_simple_routes.py
+++ aisstats/hpc/b_simple_routes.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from pytsa import SearchAgent, TimePosition, ShipType
 from functools import partial
 from pytsa.trajectories.rules import *
-
+from aisplanner.encounters.encmap import plot_coastline
 from aisstats.errchecker import COLORWHEEL
 
 SEARCHAREA = NorthSea
@@ -36,6 +36,7 @@ def plot_simple_route(tv: TargetShip, mode: str) -> None:
     
     fig, ax = plt.subplots(figsize=(8,6))
     
+    plot_coastline(ax)
     # Plot the trajectory
     ax.plot(lons,lats,color=COLORWHEEL[0],ls="-", alpha = 0.9)
     ax.scatter(lons,lats,color=COLORWHEEL[0],s=10)
@@ -43,6 +44,11 @@ def plot_simple_route(tv: TargetShip, mode: str) -> None:
     # Set labels
     ax.set_xlabel('Longitude')
     ax.set_ylabel('Latitude')
+    
+    # Set y and x limits to 1.5 times the max and min
+    # of the trajectory
+    ax.set_ylim(min(lats)-0.01,max(lats)+0.01)
+    ax.set_xlim(min(lons)-0.01,max(lons)+0.01)
     
     # Save figure
     plt.savefig(f"/home/s2075466/aisplanner/results/{tv.mmsi}_{mode}.png",dpi=300)
