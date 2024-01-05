c469ac60af31e802c251ebbf448f9f072fa276ee
nikpau <n.paulig@web.de>
2024-01-05 16:18:00 +0100

    ewrwerrewewr

--- aisstats/hpc/b_routes.py
+++ aisstats/hpc/b_routes.py
@@ -37,7 +37,7 @@ def plot_simple_route(track: list[AISMessage]) -> None:
         lons.append(msg.lon - lonmean)
         lats.append(msg.lat - latmean)
     
-    fig, ax = plt.subplots(1,2,figsize=(8,6))
+    fig, ax = plt.subplots(1,2,figsize=(8,4))
     ax: list[plt.Axes]
     
     # Plot the trajectory
