4dcab88b492f9632ffa533fbed442c9d23ec8c05
nikpau <n.paulig@web.de>
2024-01-18 10:05:01 +0100

    fgdfgd

--- aisstats/hpc/b_ecdf.py
+++ aisstats/hpc/b_ecdf.py
@@ -43,56 +43,58 @@ def quantiles(data, quantiles):
 
 SEARCHAREA = NorthSea
 
-DYNAMIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022').glob("2021*.csv"))
+for month in range(1,13):
+    if month < 10:
+        month = f"0{month}"
+    DYNAMIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022').glob(f"2021_{month}*.csv"))
+    STATIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022/msgtype5').glob(f"2021_{month}*.csv"))
 
-STATIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022/msgtype5').glob("2021*.csv"))
 
+    heading_changes = []
+    speed_changes = []
+    diff_speeds = [] # Difference between reported speed and speed calculated from positions
+    time_diffs = []
+    ddiffs = []
 
-heading_changes = []
-speed_changes = []
-diff_speeds = [] # Difference between reported speed and speed calculated from positions
-time_diffs = []
-ddiffs = []
+    SA = SearchAgent(
+        dynamic_paths=DYNAMIC_MESSAGES,
+        frame=SEARCHAREA,
+        static_paths=STATIC_MESSAGES,
+        preprocessor=partial(speed_filter, speeds= (1,30))
+    )
 
-SA = SearchAgent(
-    dynamic_paths=DYNAMIC_MESSAGES,
-    frame=SEARCHAREA,
-    static_paths=STATIC_MESSAGES,
-    preprocessor=partial(speed_filter, speeds= (1,30))
-)
+    ships = SA.get_all_ships(njobs=16,skip_tsplit=True)
+    l = len(ships)
+    for idx, ship in enumerate(ships.values()):
+        print(f"Processing ship {idx+1}/{l}")
+        for track in ship.tracks:
+            for i in range(1, len(track)):
+                heading_changes.append(
+                    heading_change(
+                        track[i-1].COG, track[i].COG
+                        )
+                )
+                speed_changes.append(abs(track[i].SOG - track[i-1].SOG))
+                rspeed = split.avg_speed(track[i-1],track[i])
+                cspeed = split.speed_from_position(track[i-1],track[i])
+                diff_speeds.append(rspeed - cspeed)
+                time_diffs.append(track[i].timestamp - track[i-1].timestamp)
+                ddiffs.append(haversine(track[i].lon,track[i].lat,track[i-1].lon,track[i-1].lat))
+                    
+    squants = quantiles(speed_changes, np.linspace(0,1,1001))
+    hquants = quantiles(heading_changes, np.linspace(0,1,1001))
+    tquants = quantiles(time_diffs, np.linspace(0,1,1001))
+    diffquants = quantiles(diff_speeds, np.linspace(0,1,1001))
+    dquants = quantiles(ddiffs, np.linspace(0,1,1001))
 
-ships = SA.get_all_ships(njobs=16,skip_tsplit=True)
-l = len(ships)
-for idx, ship in enumerate(ships.values()):
-    print(f"Processing ship {idx+1}/{l}")
-    for track in ship.tracks:
-        for i in range(1, len(track)):
-            heading_changes.append(
-                heading_change(
-                    track[i-1].COG, track[i].COG
-                    )
-            )
-            speed_changes.append(abs(track[i].SOG - track[i-1].SOG))
-            rspeed = split.avg_speed(track[i-1],track[i])
-            cspeed = split.speed_from_position(track[i-1],track[i])
-            diff_speeds.append(rspeed - cspeed)
-            time_diffs.append(track[i].timestamp - track[i-1].timestamp)
-            ddiffs.append(haversine(track[i].lon,track[i].lat,track[i-1].lon,track[i-1].lat))
-                
-squants = quantiles(speed_changes, np.linspace(0,1,1001))
-hquants = quantiles(heading_changes, np.linspace(0,1,1001))
-tquants = quantiles(time_diffs, np.linspace(0,1,1001))
-diffquants = quantiles(diff_speeds, np.linspace(0,1,1001))
-dquants = quantiles(ddiffs, np.linspace(0,1,1001))
-
-# Save the quantiles
-with open('/home/s2075466/aisplanner/results/squants.pkl', 'wb') as f:
-    pickle.dump(squants, f)
-with open('/home/s2075466/aisplanner/results/hquants.pkl', 'wb') as f:    
-    pickle.dump(hquants, f)
-with open('/home/s2075466/aisplanner/results/tquants.pkl', 'wb') as f:
-    pickle.dump(tquants, f)
-with open('/home/s2075466/aisplanner/results/diffquants.pkl', 'wb') as f:
-    pickle.dump(diffquants, f)
-with open('/home/s2075466/aisplanner/results/dquants.pkl', 'wb') as f:
-    pickle.dump(dquants, f)
+    # Save the quantiles
+    with open(f'/home/s2075466/aisplanner/results/squants_{month}.pkl', 'wb') as f:
+        pickle.dump(squants, f)
+    with open(f'/home/s2075466/aisplanner/results/hquants_{month}.pkl', 'wb') as f:    
+        pickle.dump(hquants, f)
+    with open(f'/home/s2075466/aisplanner/results/tquants_{month}.pkl', 'wb') as f:
+        pickle.dump(tquants, f)
+    with open(f'/home/s2075466/aisplanner/results/diffquants_{month}.pkl', 'wb') as f:
+        pickle.dump(diffquants, f)
+    with open(f'/home/s2075466/aisplanner/results/dquants_{month}.pkl', 'wb') as f:
+        pickle.dump(dquants, f)
