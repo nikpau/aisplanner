b08ac10e83dc214a420ed3fad93be4e9311c3e2d
nikpau <n.paulig@web.de>
2024-01-24 16:21:16 +0100

    fg

--- aisstats/hpc/b_3d_avg_cmplx.py
+++ aisstats/hpc/b_3d_avg_cmplx.py
@@ -14,10 +14,14 @@ from pytsa.trajectories.rules import *
 import logging
 import pickle
 import multiprocessing as mp
+import sys
+
+sd = sys.argv[1]
+minlen = sys.argv[2]
 
 # Set logging level to warning
 SDS = np.array([0.01,0.015,0.02,0.025,0.03,0.04,0.05,0.1,0.2,0.3,0.4,0.5])
-MINLENS = np.array([0,5,10,15,20,30,40,50,60,70,80,90,100,200,300,400,500])
+MINLENS = np.array([0,5,10,15,20,30,40,50,60,80,100,200,400,500])
 
 def average_complexity(ships: dict[int,TargetShip]):
     """
@@ -25,36 +29,31 @@ def average_complexity(ships: dict[int,TargetShip]):
     enclosed between three consecutive messages 
     for several standard deviations.
     """
-    avg_smthness = np.zeros((len(MINLENS),len(SDS)))
-    
-    for i, minlen in enumerate(MINLENS): 
-        for j, sd in enumerate(SDS):
-            recipe = Recipe(
-                    partial(too_few_obs,n=minlen),
-                    partial(spatial_deviation,sd=sd)
-                )
-            inpsctr = pytsa.Inspector(
-                    data=ships,
-                    recipe=recipe
-                )
-            print("Inspecting...")
-            acc, rej = inpsctr.inspect(njobs=24)
-            del acc
-            smthness = []
-            for ship in rej.values():
-                for track in ship.tracks:
-                    smthness.append(inspect.average_smoothness(track))
-            avg_smthness[i,j] = np.mean(smthness)
+    recipe = Recipe(
+            partial(too_few_obs,n=minlen),
+            partial(spatial_deviation,sd=sd)
+        )
+    inpsctr = pytsa.Inspector(
+            data=ships,
+            recipe=recipe
+        )
+    print("Inspecting...")
+    acc, rej = inpsctr.inspect(njobs=1)
+    del acc
+    smthness = []
+    for ship in rej.values():
+        for track in ship.tracks:
+            smthness.append(inspect.average_smoothness(track))
             
     # Save the results
-    with open("/home/s2075466/aisplanner/results/avg_smoothness.pkl","wb") as f:
-        pickle.dump(avg_smthness,f)
+    with open(f"/home/s2075466/aisplanner/results/avg_smoothness_{sd}_{minlen}.pkl","wb") as f:
+        pickle.dump(np.mean(smthness),f)
 
 if __name__ == "__main__":
     SEARCHAREA = NorthSea
 
-    DYNAMIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022').glob("2021_07*.csv"))
-    STATIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022/msgtype5').glob("2021_07*.csv"))
+    DYNAMIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022').glob("2021*.csv"))
+    STATIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022/msgtype5').glob("2021*.csv"))
 
     SA = SearchAgent(
             dynamic_paths=DYNAMIC_MESSAGES,
