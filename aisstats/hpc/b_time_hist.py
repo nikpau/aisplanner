8c5d1f513c817d856425f36e546e8dbeb04557c7
nikpau <n.paulig@web.de>
2024-04-02 09:31:49 +0200

    fdsfdfdssfd

--- aisstats/hpc/b_time_hist.py
+++ aisstats/hpc/b_time_hist.py
@@ -16,13 +16,14 @@ from matplotlib import pyplot as plt
 from aisplanner.encounters.main import NorthSea
 from pathlib import Path
 from pytsa import SearchAgent
+from matplotlib import patches
 
 DYNAMIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022').glob("2021*.csv"))
 STATIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022/msgtype5').glob("2021*.csv"))
 
 SEARCHAREA = NorthSea
 
-def plot_td_histogram(speeds: np.ndarray,
+def plot_td_histogram(tdiffs: np.ndarray,
                         savename: str) -> None:
     """
     Plots a histogram of the temporal differences 
@@ -30,10 +31,35 @@ def plot_td_histogram(speeds: np.ndarray,
     """
     
     fig, ax = plt.subplots(figsize=(6,4))
-    ax.hist(speeds, bins=100, color=COLORWHEEL[0])
+
+    # Quantiles of time diffs
+    qs = np.quantile(
+        tdiffs,
+        [0.99,0.95,0.90]
+    )
+    tdiffs500 = tdiffs[tdiffs <= 500]
+    ax.hist(tdiffs500, bins=100, color=COLORWHEEL[0])
+
+
+    q_labels = [
+        f"1% larger {qs[0]:.2f} s",
+        f"5% larger {qs[1]:.2f} s",
+        f"10% larger {qs[2]:.2f} s"
+    ]
+
+    # Heading Quantiles as vertical lines
+    hl11 = ax.axvline(qs[0],color=COLORWHEEL[0],label=q_labels[0],ls="--")
+    hl21 = ax.axvline(qs[1],color=COLORWHEEL[0],label=q_labels[1],ls="-.")
+    hl31 = ax.axvline(qs[2],color=COLORWHEEL[0],label=q_labels[2],ls=":")
+    
     ax.set_xlabel("Time difference [s]")
+    ax.set_xlim(0,500)
     ax.set_ylabel("Number of observations")
     ax.set_yscale('log')
+
+    # Legend with heading
+    ax.legend(handles=[hl11,hl21,hl31])    # Heading Quantiles as vertical lines
+
     ax.set_axisbelow(True)
     plt.tight_layout()
     plt.savefig(savename,dpi=400)
@@ -52,8 +78,6 @@ if __name__ == "__main__":
         for track in ship.tracks:
             for i in range(1,len(track)):
                 diff = track[i].timestamp - track[i-1].timestamp
-                if diff > 500:
-                    continue
                 temp_diffs.append(diff)
-
+    
     plot_td_histogram(temp_diffs, savename="/home/s2075466/aisplanner/results/td_hist_21.pdf")
