0554925368179a388afe62d2e9f66ad383ad6d50
nikapu <n.paulig@web.de>
2024-08-01 08:56:20 +0200

    sdfsdf

--- aisstats/hpc/b_ecdf_ship_type.py
+++ aisstats/hpc/b_ecdf_ship_type.py
@@ -61,7 +61,7 @@ SA = SearchAgent(
     preprocessor=partial(speed_filter, speeds= (1,30))
 )
 
-ships = SA.extract_all(njobs=24,skip_tsplit=True)
+ships = SA.extract_all(njobs=48,skip_tsplit=True)
 l = len(ships)
 for idx, ship in enumerate(ships.values()):
     print(f"Processing ship {idx+1}/{l}")
