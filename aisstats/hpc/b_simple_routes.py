9c76b1441f944ce70fb3c011fe8cbf5f44239cd8
nikapu <n.paulig@web.de>
2024-08-01 09:15:32 +0200

    fghfgh

--- aisstats/hpc/b_simple_routes.py
+++ aisstats/hpc/b_simple_routes.py
@@ -5,7 +5,8 @@ for the AIS data set
 from glob import glob
 from pathlib import Path
 from matplotlib import pyplot as plt
-from pytsa.decode import filedescriptor as fd
+from pytsa.decoder import filedescriptor as fd
+import pytsa
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
@@ -37,14 +38,17 @@ def plot_simple_route(tv: TargetShip, mode: str) -> None:
     
     fig, ax = plt.subplots(figsize=(8,6))
     
-    plot_coastline(SEARCHAREA,ax)
+    plot_coastline(
+        datapath="/home/s2075466/aisplanner/data/geometry",
+        extent = SEARCHAREA,
+        ax = ax)
     # Plot the trajectory
     ax.plot(lons,lats,color=COLORWHEEL_MAP[0],ls="-", alpha = 0.9)
     ax.scatter(lons,lats,color=COLORWHEEL_MAP[0],s=10)
         
     # Set labels
-    ax.set_xlabel('Longitude')
-    ax.set_ylabel('Latitude')
+    ax.set_xlabel('Longitude [°]')
+    ax.set_ylabel('Latitude [°]')
     
     # Set y and x limits to 1.5 times the max and min
     # of the trajectory
@@ -52,7 +56,7 @@ def plot_simple_route(tv: TargetShip, mode: str) -> None:
     ax.set_xlim(min(lons)-0.001,max(lons)+0.001)
     
     # Save figure
-    plt.savefig(f"/home/s2075466/aisplanner/results/{tv.mmsi}_{mode}.png",dpi=300)
+    plt.savefig(f"/home/s2075466/aisplanner/results/rejected/{tv.mmsi}_{mode}.png",dpi=300)
     plt.close()
 
 if __name__ == "__main__":
@@ -64,7 +68,10 @@ if __name__ == "__main__":
 
     ships = SA.extract_all(njobs=16,skip_tsplit=True)
     
-    tvs = list(ships.values())
+    inspcr = pytsa.trajectories.inspect.Inspector(ships,ExampleRecipe)
+    acc , rej = inspcr.inspect()
+    
+    tvs = list(rej.values())
     for i in range(60,200):
         tv = tvs[i]
         plot_simple_route(tv, "rejected")
