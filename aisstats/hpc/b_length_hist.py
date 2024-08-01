467898f10eb652e35ead0e36889fbd4aa509bbed
nikapu <n.paulig@web.de>
2024-08-01 15:02:37 +0200

    sdfsdf

---
+++ aisstats/hpc/b_length_hist.py
@@ -0,0 +1,46 @@
+"""
+HPC script for calculating descriptive data
+for the AIS data set
+"""
+from pathlib import Path
+
+import numpy as np
+from aisplanner.encounters.main import NorthSea
+from aisstats.errchecker import plot_speed_histogram
+from pathlib import Path
+from pytsa.structs import ShipType
+import ciso8601
+import pandas as pd
+import multiprocessing as mp
+import matplotlib.pyplot as plt
+
+
+def _date_transformer(datefile: Path) -> float:
+    return ciso8601.parse_datetime(datefile.stem.replace("_", "-"))
+
+STATIC_MESSAGES = list(Path('/home/s2075466/ais/decoded/jan2020_to_jun2022/msgtype5').glob("2021_07_01.csv"))
+
+def load_and_count(path: Path) -> int:
+    stdict = {s:[] for s in ShipType}
+    print(f"Processing {path}")
+    df = pd.read_csv(path,usecols=['to_bow',"to_stern","ship_type"])
+    df["length"] = df["to_bow"] + df["to_stern"]
+    
+    for row in df.itertuples():
+        stdict[ShipType.from_value(row.ship_type)].append(row.length)
+    
+    return stdict
+
+if __name__ == "__main__":
+    with mp.Pool(23) as pool:
+        results = pool.map(load_and_count, STATIC_MESSAGES)
+    
+    # Join all dicts into one
+    stdict = {s:[] for s in ShipType}
+    for res in results:
+        for s in ShipType:
+            stdict[s].extend(res[s])
+            
+    fig, ax = plt.subplots(figsize=(12,6))
+    for s in ShipType:
+        ax.hist(stdict[s], bins=100, alpha=0.5, label=s.name)
