ee7720ff68dab760d3d1cc78867a226cb1a070b8
nikapu <n.paulig@web.de>
2024-08-01 10:43:30 +0200

    sdf

--- aisstats/hpc/b_simple_routes.py
+++ aisstats/hpc/b_simple_routes.py
@@ -18,7 +18,7 @@ from pytsa import SearchAgent, TimePosition, ShipType
 from functools import partial
 from pytsa.trajectories.rules import *
 from aisplanner.encounters.encmap import plot_coastline
-from aisstats.errchecker import COLORWHEEL_MAP
+from aisstats.errchecker import COLORWHEEL
 import geopandas as gpd
 
 SEARCHAREA = NorthSea
@@ -43,8 +43,8 @@ def plot_simple_route(tv: TargetShip, mode: str) -> None:
     #     extent = SEARCHAREA,
     #     ax = ax)
     # Plot the trajectory
-    ax.plot(lons,lats,color=COLORWHEEL_MAP[0],ls="-", alpha = 0.9)
-    ax.scatter(lons,lats,color=COLORWHEEL_MAP[0],s=10)
+    ax.plot(lons,lats,color=COLORWHEEL[0],ls="-", alpha = 0.9)
+    ax.scatter(lons,lats,color=COLORWHEEL[0],s=10)
         
     # Set labels
     ax.set_xlabel('Longitude [°]')
@@ -52,8 +52,10 @@ def plot_simple_route(tv: TargetShip, mode: str) -> None:
     
     # Set y and x limits to 1.5 times the max and min
     # of the trajectory
-    ax.set_ylim(min(lats)-0.001,max(lats)+0.001)
-    ax.set_xlim(min(lons)-0.001,max(lons)+0.001)
+    yext = (max(lats) - min(lats)) * 0.05
+    xext = (max(lons) - min(lons)) * 0.05
+    ax.set_ylim(min(lats)-yext,max(lats)+yext)
+    ax.set_xlim(min(lons)-xext,max(lons)+xext)
     
     # Save figure
     plt.savefig(f"/home/s2075466/aisplanner/results/rejected/{tv.mmsi}_{mode}.png",dpi=300)
