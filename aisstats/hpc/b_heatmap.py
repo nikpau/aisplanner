bb55a1c9332e430b01d573a2771ff6095305a840
nikapu <n.paulig@web.de>
2024-08-08 08:09:42 +0200

    vfhfgh

--- aisstats/hpc/b_heatmap.py
+++ aisstats/hpc/b_heatmap.py
@@ -21,7 +21,7 @@ ships = SA.extract_all(njobs=32)
 
 R = Recipe(
     partial(too_few_obs, n=50),
-    partial(convex_hull_area, area=3e4)
+    partial(convex_hull_area, area=5e3)
 )
 from pytsa.trajectories import Inspector
 inspctr = Inspector(
@@ -29,27 +29,18 @@ inspctr = Inspector(
     recipe=R
 )
 accepted, rejected = inspctr.inspect(njobs=1)
-# print(
-#     f"No. of accepted trajectories: {sum([len(t.tracks) for t in accepted.values()])}\n"
-#     f"No. of rejected trajectories: {sum([len(t.tracks) for t in rejected.values()])}\n"
-#     f"Total no. of accepted observations: {sum([sum(len(tr) for tr in t.tracks) for t in accepted.values()])}\n"
-#     f"Total no. of rejected observations: {sum([sum(len(tr) for tr in t.tracks) for t in rejected.values()])}\n"
-# )
+print(
+    f"No. of accepted trajectories: {sum([len(t.tracks) for t in accepted.values()])}\n"
+    f"No. of rejected trajectories: {sum([len(t.tracks) for t in rejected.values()])}\n"
+    f"Total no. of accepted observations: {sum([sum(len(tr) for tr in t.tracks) for t in accepted.values()])}\n"
+    f"Total no. of rejected observations: {sum([sum(len(tr) for tr in t.tracks) for t in rejected.values()])}\n"
+)
 binned_heatmap(accepted, SEARCHAREA, savename=f"/home/s2075466/aisplanner/results/maps/hm_accepted_all_21.png")
 binned_heatmap(rejected, SEARCHAREA, savename=f"/home/s2075466/aisplanner/results/maps/hm_rejected_all_21.png")
 # Split up accepted and rejected trajectories
-# to only contain ships of type CARGO, TANKER,
-# PASSENGER and FISHING
-types = [t for t in ShipType]
-names = [t.name for t in types]
-expanded = []
-for t in types:
-    if isinstance(t.value, int):
-        expanded.append([t.value])
-    else:
-        expanded.append(list(t.value))
-for i,t in enumerate(expanded):
-    a = {mmsi:s for mmsi,s in accepted.items() if any(st in t for st in s.ship_type)}
-    r = {mmsi:s for mmsi,s in rejected.items() if any(st in t for st in s.ship_type)}
-    binned_heatmap(a, SEARCHAREA, savename=f"/home/s2075466/aisplanner/results/maps/hm_accepted_{names[i]}_21.png")
-    binned_heatmap(r, SEARCHAREA, savename=f"/home/s2075466/aisplanner/results/maps/hm_rejected_{names[i]}_21.png")
+
+for s in ShipType:
+    a = {mmsi:s for mmsi,s in accepted.items() if s.ship_type == s}
+    r = {mmsi:s for mmsi,s in rejected.items() if s.ship_type == s}
+    binned_heatmap(a, SEARCHAREA, savename=f"/home/s2075466/aisplanner/results/maps/hm_accepted_{s.name}_21.png")
+    binned_heatmap(r, SEARCHAREA, savename=f"/home/s2075466/aisplanner/results/maps/hm_rejected_{s.name}_21.png")
